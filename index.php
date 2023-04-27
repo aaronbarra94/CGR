33e9fd28899ddba12ea32fe8ad64f948ea10535d
Aaron Barra <aar.araya@duocuc.cl>
2023-04-26 22:02:33 -0400

    update metodo post index.php

--- index.php
+++ index.php
@@ -13,30 +13,31 @@ require 'src/PHPMailer/SMTP.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host = 'mail.constructoracgr.cl';                     //Set the SMTP server to send through
+        $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+        $mail->Username = 'contacto@constructoracgr.cl';                     //SMTP username
+        $mail->Password = 'CGRContacto1000.';                               //SMTP password
+        $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+        $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        //Recipients
+        $mail->setFrom('contacto@constructoracgr.cl', 'Cotizacion');
+        $mail->addAddress('contacto@constructoracgr.cl', 'Cotizacion');     //Add a recipient
+        //Contador
+        $lastQuoteNumber = file_get_contents('src/cotizacion.txt');
+        //Incrementa el número en uno
+        $newQuoteNumber = $lastQuoteNumber + 1;
+        //Guarda el nuevo número en el archivo
+        file_put_contents('src/cotizacion.txt', $newQuoteNumber);
 
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Cotizacion #' . $newQuoteNumber . ' de ' . $_POST['name'];
+        $mail->Body = 'Numero contacto: ' . $_POST['phone'] . "\r\n" . 'Mensaje: ' . $_POST['message'];
+
+        $mail->send();
+        echo 'Enviado correctamente';
+}
     //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.constructoracgr.cl';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'contacto@constructoracgr.cl';                     //SMTP username
-    $mail->Password   = 'CGRContacto1000.';                               //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    //Recipients
-    $mail->setFrom('contacto@constructoracgr.cl', 'Cotizacion');
-    $mail->addAddress('contacto@constructoracgr.cl', 'Cotizacion');     //Add a recipient
-    //Contador
-    $lastQuoteNumber = file_get_contents('src/cotizacion.txt');
-    //Incrementa el número en uno
-    $newQuoteNumber = $lastQuoteNumber + 1;
-    //Guarda el nuevo número en el archivo
-    file_put_contents('src/cotizacion.txt', $newQuoteNumber);
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Cotizacion #'.$newQuoteNumber.' de '.$_POST['name'];
-    $mail->Body    = 'Numero contacto: '.$_POST['phone']."\r\n".'Mensaje: '.$_POST['message'];
-
-    $mail->send();
-    echo 'Enviado correctamente';
